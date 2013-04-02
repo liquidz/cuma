5b93e25035ada622415b6e490a439d8f54932138
liquidz <liquidz.uo@gmail.com>
2013-04-03 00:53:53 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# semplate
+
+A Clojure library designed to ... well, that part is up to you.
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright (C) 2013 Masashi Iizuka([@uochan](http://twitter.com))
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject semplate "0.0.1"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [evalive "1.1.0"]])
